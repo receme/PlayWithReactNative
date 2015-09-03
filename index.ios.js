b2b853677dfefedf70b1725b35c2489691bdaeb0
receme <tahmid1989@gmail.com>
2015-09-04 01:34:12 +0600

    project init, show dummy movie info with image

---
+++ index.ios.js
@@ -0,0 +1,60 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ */
+'use strict';
+
+var React = require('react-native');
+var {
+  AppRegistry,
+  Image,
+  StyleSheet,
+  Text,
+  View,
+} = React;
+
+var MOCKED_MOVIES_DATA = [
+	{title: 'Movie Title', year: '2015',
+	 posters: {thumbnail: 'http://i.imgur.com/UePbdph.jpg'}},
+];
+
+
+var PlayWithReactNative = React.createClass({
+	render:function(){
+		var movie = MOCKED_MOVIES_DATA[0];
+
+		return(
+			<View style={styles.container}>
+        	<Text>{movie.title}</Text>
+        	<Text>{movie.year}</Text>
+        	<Image 
+        		source={{uri: movie.posters.thumbnail}}
+        		style={styles.thumbnail} 
+        	/>
+        	
+      		</View>
+		)
+	}
+})
+
+
+
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  thumbnail: {
+    width: 53,
+    height: 81,
+  }
+});
+
+AppRegistry.registerComponent('PlayWithReactNative', () => PlayWithReactNative);
